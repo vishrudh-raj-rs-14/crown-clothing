7733620817132c8b27ebdb829f250f52582afb75
Vishrudh Raj.R.S <vishrudhraj.r.s@Vishrudhs-MacBook-Pro.local>
2022-10-14 11:59:35 +0530

    Minor Changes Adding Fonts

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import "./categories.styles.scss";
 import Directory from "./components/directory/directory.component";
 
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: "hats",
+      title: "Hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
-      title: "jackets",
+      title: "Jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "sneakers",
+      title: "Sneakers",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "womens",
+      title: "Women",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "mens",
+      title: "Men",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
